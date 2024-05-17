c1e9a3dccf06edbaafac4dbdd218190d4ccd24a6
Yilu W <luwyl@yahoo.com>
2024-05-17 10:20:43 +0000

    update to newest version

--- part_b/agentAB/habp.py
+++ part_b/agentAB/habp.py
@@ -7,9 +7,11 @@ from collections import namedtuple
 
 import numpy as np
 
-from .board import Board, BOARD_N
+from .board import Board, BOARD_N, Direction, Coord, CellState
 from referee.game import PlaceAction, PlayerColor
 from referee.agent.resources import CountdownTimer
+from . import _testing
+from referee.run import game_delay
 
 GameState = namedtuple('GameState', 'to_move, utility, board, moves')
 StochasticGameState = namedtuple('StochasticGameState', 'to_move, utility, board, moves, chance')
@@ -19,29 +21,9 @@ StochasticGameState = namedtuple('StochasticGameState', 'to_move, utility, board
 MAX_TURN = 150 
 TURN_THRESHOLD = MAX_TURN * 0.8 
 
-def action_utility(board:Board, action:PlaceAction, player:PlayerColor) -> int: 
-    '''Find the utility of an action for sorting the action for maximize pruning 
-    '''
-
-    def diff_row_col_occupied(player:PlayerColor) -> int: 
-        player_occupied = (board._player_occupied_coords(player)) 
-        opponent_occupied = (board._player_occupied_coords(player.opponent)) 
-        player_occupied_row = set(map(lambda coord: coord.r, player_occupied))
-        player_occupied_col = set(map(lambda coord: coord.c, player_occupied))
-        opponent_occupied_row = set(map(lambda coord: coord.r, opponent_occupied))
-        opponent_occupied_col = set(map(lambda coord: coord.c, opponent_occupied))
-        return len(player_occupied_row) + len(player_occupied_col) \
-            - len(opponent_occupied_row) - len(opponent_occupied_col)
-    
-    board.apply_action(action) 
-
-    # Calculate the weighted sum of the utility components for `player` (i.e. the agent using the habp) of the new action played 
-    utility = diff_row_col_occupied(player) # TODO - add more component to make the weighted sum more accurate 
-
-    board.undo_action()
-
-    return utility
-
+def print(*values): 
+    ''' Change this function name to `_print` if want to print out debug messages '''
+    pass 
 
 def alpha_beta_cutoff_search(board:Board):
     """Search game to determine best action; use alpha-beta pruning.
@@ -50,17 +32,22 @@ def alpha_beta_cutoff_search(board:Board):
 
     def cutoff_test(board:Board, depth):
         # If the board is relatively empty, simply check until a certain depth  
-        SPARSE_THREASHOLD = BOARD_N * BOARD_N / 3
-        if len(board._empty_coords()) > SPARSE_THREASHOLD: 
+        if board.game_over: 
+            return True
+        
+        if len(board._empty_coords()) > BOARD_N * BOARD_N / 3: 
             return depth > 1
+        if len(board._empty_coords()) > BOARD_N * BOARD_N / 4:
+            return depth > 2
 
         # TODO - if the state of the game is unstable, go deeper? 
-        # if board.is_stable(): 
+        # if not board.is_stable(): 
         #     return depth > 5
         # else: 
-        return depth > 4 or board.game_over
+        return depth > 4 
 
-    def eval_fn(board:Board, player:PlayerColor) -> int: 
+    def eval_fn(board:Board, player:PlayerColor) -> float: 
+        print(_testing.prefix(), "evaluate start")
         # If there is a winner, give the result straight away 
         if board.winner_color == player.opponent: 
             return -1 
@@ -68,83 +55,183 @@ def alpha_beta_cutoff_search(board:Board):
             return 1,000,000,000,000
 
         # Otherwise, evaluate the game state 
-        num_actions = len(board.get_legal_actions(player))
-        num_opponent_actions = len(board.get_legal_actions(player.opponent))
-        extra_num_actions = num_actions - num_opponent_actions
 
-        num_player_occupied = len(board._player_occupied_coords(player))
-        num_opponent_occupied = len(board._player_occupied_coords(player.opponent))
-        extra_num_occupied = num_player_occupied - num_opponent_occupied
+        # Find the difference in the number of actions 
+        extra_num_actions = diff_legal_actions(board, player)
+
+        # Find the difference in the number of empty cells reachable that can be used as an action 
+        extra_num_reachable = diff_reachable_valid_empty_cell(board, player)
+
+        # Find the difference in the number of cells occupied 
+        extra_num_occupied = diff_cells_occupied(board, player)
 
         if board.turn_count < TURN_THRESHOLD: 
-            utility = extra_num_actions + 0.1 * extra_num_occupied
+            # Far away from turns limitation 
+            utility = (
+                extra_num_actions +         \
+                extra_num_reachable +       \
+                extra_num_occupied * 0.1 
+            )
         else: 
             # If about to reach turns limit, evalution also include the number of cells occupied 
-            utility = extra_num_actions + \
-                (board.turn_count - TURN_THRESHOLD) * 0.5 * extra_num_occupied
+            turns_exceed_threshold = board.turn_count - TURN_THRESHOLD
+            utility = (
+                extra_num_actions +                                 \
+                extra_num_reachable +                               \
+                extra_num_occupied * turns_exceed_threshold * 0.5 
+            )
         
+        print(_testing.prefix(), "evaluate end")
         return utility
 
-    # def ___DISCARD_action_utility(board, action) -> int: 
-    #     print("action_utility")
-    #     timer = CountdownTimer(time_limit=2)
-    #     timer.__enter__()
-    #     board.apply_action(action) 
-    #     utility = eval_fn(board)
-    #     board.undo_action()
-    #     timer.__exit__(None, None, None)
-    #     return utility 
-
     # Functions used by alpha_beta
-    def max_value(board:Board, alpha, beta, depth):
+    def max_value(board:Board, alpha, beta, depth) -> float:
         if cutoff_test(board, depth):
             return eval_fn(board, player)
         v = -np.inf
         #for a in board.get_legal_actions():
-        for a in sorted(board.get_legal_actions(), key=lambda action: action_utility(board, action, player), reverse=True): 
-            #print(f"apply action {a} in max_value() with depth {depth}")
-            board.apply_action(a) 
+        for action in sorted(board.get_legal_actions(), key=lambda action: action_utility(board, action, player), reverse=True)[:10]: 
+            print(_testing.prefix(27), f"max-val apply action {action} in max_value() with depth {depth}")
+            board.apply_action(action)
             v = max(v, min_value(board, alpha, beta, depth + 1))
+            board.undo_action()
             if v >= beta:
-                board.undo_action()
                 return v
             alpha = max(alpha, v)
-            board.undo_action()
         return v
 
-    def min_value(board:Board, alpha, beta, depth):
+    def min_value(board:Board, alpha, beta, depth) -> float:
         if cutoff_test(board, depth):
             return eval_fn(board, player)
         v = np.inf
         # for a in board.get_legal_actions():
-        for a in sorted(board.get_legal_actions(), key=lambda action: action_utility(board, action, player), reverse=True):
-            #print(f"apply action {a} in min_value() with depth {depth}")
-            board.apply_action(a)
+        for action in sorted(board.get_legal_actions(), key=lambda action: action_utility(board, action, player))[:10]:
+            print(_testing.prefix(27), f"min-val apply action {action} in max_value() with depth {depth}")
+            board.apply_action(action)
             v = min(v, max_value(board, alpha, beta, depth + 1))
+            board.undo_action()
             if v <= alpha:
-                board.undo_action()
                 return v
             beta = min(beta, v)
-            board.undo_action()
         return v
 
     # Body of alpha_beta_cutoff_search starts here:
     best_score = -np.inf
     beta = np.inf
     best_action = None
-    for a in board.get_legal_actions():
-        timer = CountdownTimer(time_limit=1,tolerance=20)
-        timer.__enter__()
 
-        board.apply_action(a)
+    legal_actions = board.get_legal_actions()
+    print(_testing.prefix(17), f"number of legal actions = {len(legal_actions)}")
+
+    MIN_ACTIONS_TEST = 30
+    if len(legal_actions) < MIN_ACTIONS_TEST: 
+        random_index = range(len(legal_actions))
+    else: 
+        random_index:set[int] = set([random.randint(0, len(legal_actions)-1) for _ in range(MIN_ACTIONS_TEST)])
+    print(_testing.prefix(17), random_index) 
+
+    for action in [legal_actions[i] for i in random_index]:
+        # timer = CountdownTimer(time_limit=1,tolerance=10)
+        # timer.__enter__()
+
+        # Apply the action, evaluate alpha and beta, then undo the action 
+        board.apply_action(action)     
         v = min_value(board, best_score, beta, 1)
         if v > best_score:
             best_score = v
-            best_action = a
+            best_action = action
         board.undo_action()
-
-        timer.__exit__(None, None, None)
+        # timer.__exit__(None, None, None)
     return best_action
+
+
+def action_utility(board:Board, action:PlaceAction, player:PlayerColor) -> int: 
+    '''Find the utility of an action for sorting the action for maximize pruning 
+    '''
+
+    board.apply_action(action)
+
+    # Calculate the weighted sum of the utility components for `player` (i.e. the agent using the habp) 
+    utility = (
+        diff_row_col_occupied(board, player) * 0.8 +    \
+        #diff_cells_occupied(new_board, player) * 0.5 +      \
+        diff_reachable_valid_empty_cell(board, player)
+    )
+
+    board.undo_action()
+    return utility
+
+
+# =============== Utility Evaluation Fundctions ==================== 
+
+def diff_cells_occupied(board:Board, player:PlayerColor) -> int:
+    num_player_occupied = board._player_token_count(player)
+    num_opponent_occupied = board._player_token_count(player.opponent)
+    return num_player_occupied - num_opponent_occupied
+
+
+def diff_legal_actions(board:Board, player:PlayerColor) -> int: 
+    ''' Find the difference in the number of legal actions 
+        between the player and the opponent. 
+    '''
+    num_actions = len(board.get_legal_actions(player))
+    num_opponent_actions = len(board.get_legal_actions(player.opponent))
+    return num_actions - num_opponent_actions
+
+
+def diff_reachable_valid_empty_cell(board:Board, player:PlayerColor) -> int: 
+    ''' Find the difference in the number of valid empty cells reachable 
+        between the player and the opponent. 
+        A cell is valid if it is connected to at least 3 other empty cells. 
+    '''
+    def empty_connected(board:Board, empty:Coord) -> set[Coord]:
+        ''' Return the empty cells connected to the `empty` cell
+        '''
+        frontier = [empty]
+        connected = {empty}
+        while frontier: 
+            visiting = frontier.pop()
+            for adjacent in [visiting.__add__(direction) for direction in Direction]: 
+                if board._cell_empty(adjacent) and adjacent not in connected: 
+                    frontier.append(adjacent)
+                    connected.add(adjacent)
+        return connected
+    
+    def num_valid_reachable_cells(board:Board, player:PlayerColor) -> int: 
+        reachable = 0
+        visited = set()
+        occupied = board._player_occupied_coords(player)
+        for cell in occupied: 
+            for adjacent in [cell.__add__(direction) for direction in Direction]: 
+                if board._cell_empty(adjacent) and adjacent not in visited: 
+                    connected = empty_connected(board, adjacent)
+                    # _testing.show(connected, description="new empty region connected")
+                    # _testing.show(visited, description="previously visited empty region")
+                    assert(len(visited.intersection(connected)) == 0)  # `connected` should be a new region that has not been visited 
+                    visited.update(connected)
+                    
+                    # Only add valid cell count to the output 
+                    if len(connected) >= 4: 
+                        reachable += len(connected)
+        return reachable
+    
+    num_empty_player_reachable = num_valid_reachable_cells(board, player)
+    num_empty_opponent_reachable = num_valid_reachable_cells(board, player.opponent)
+    return num_empty_player_reachable - num_empty_opponent_reachable
+
+
+def diff_row_col_occupied(board:Board, player:PlayerColor) -> int: 
+    ''' Find the difference in the sum of the number of rows and columns occupied
+        between the player and the opponent. 
+    '''
+    player_occupied = (board._player_occupied_coords(player)) 
+    opponent_occupied = (board._player_occupied_coords(player.opponent)) 
+    player_occupied_row = set(map(lambda coord: coord.r, player_occupied))
+    player_occupied_col = set(map(lambda coord: coord.c, player_occupied))
+    opponent_occupied_row = set(map(lambda coord: coord.r, opponent_occupied))
+    opponent_occupied_col = set(map(lambda coord: coord.c, opponent_occupied))
+    return len(player_occupied_row) + len(player_occupied_col) \
+        - len(opponent_occupied_row) - len(opponent_occupied_col)
 
 
 # ______________________________________________________________________________
@@ -170,7 +257,6 @@ def query_player(game, state):
 
 def alpha_beta_player(game, state):
     return alpha_beta_cutoff_search(state, game)
-
 
 # ______________________________________________________________________________
 # Some Sample Games
