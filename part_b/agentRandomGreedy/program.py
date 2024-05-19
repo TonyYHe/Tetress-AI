ae9a4765acd79742ced71737f668767e9c607fbe
TonyYHe <tyhe@student.unimelb.edu.au>
2024-05-19 13:56:59 +0000

    Add testing results

---
+++ part_b/agentRandomGreedy/program.py
@@ -0,0 +1,45 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part B: Game Playing Agent
+
+from referee.game import *
+from utils.board import Board
+from utils.node import Node
+from utils.orderchildren import *
+
+
+class Agent:
+    """
+    This class is the "entry point" for your agent, providing an interface to
+    respond to various Tetress game events.
+    """
+
+    def __init__(self, color: PlayerColor, **referee: dict):
+        """
+        This constructor method runs when the referee instantiates the agent.
+        Any setup and/or precomputation should be done here.
+        """
+        self.board = Board(initial_player=PlayerColor.RED)
+        self.color = color
+        self.transposition_table = TranspositionTable()
+
+    def action(self, **referee: dict) -> Action:
+        """
+        This method is called by the referee each time it is the agent's turn
+        to take an action. It must always return an action object. 
+        """
+        if self.board._turn_count <= 15:
+            return random.choice(self.board.get_legal_actions())
+        
+        node = Node(self.board, self.color)
+        children = node.get_all_children(self.board)
+        children = OrderChildren.order_children(self.board, children, self.color, self.transposition_table)
+        return children[0].parent_action
+       
+
+    def update(self, color: PlayerColor, action: Action, **referee: dict):
+        """
+        This method is called by the referee after an agent has taken their
+        turn. You should use it to update the agent's internal game state. 
+        """
+        self.board.apply_action(action)
+    
--- part_b/run_games2.sh
+++ part_b/run_games2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-output_file="_testing_output/habp-vs-greedy.txt"
+output_file="_testing_output/habp-token-legalactions-vs-randomgreedy.txt"
 
 for i in $(seq 1 100)
 do
-    echo "GAME ${i}: $(python3 -m referee -s 250 -t 180 agent agentGreedy -v 0)" | tee -a "$output_file"
+    echo "GAME ${i}: $(python3 -m referee -s 250 -t 180 agent agentRandomGreedy -v 0)" | tee -a "$output_file"
 done
