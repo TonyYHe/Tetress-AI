9f13e0094bf3dfc2728d16badd662f5d4e61c170
Yilu W <luwyl@yahoo.com>
2024-05-20 07:26:14 +0000

    try testing usage

--- part_b/run_games.sh
+++ part_b/run_games.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-output_file="_testing_final/habp-vs-random.txt"
+output_file="_testing_final/habp-vs-greedy.txt"
 
 for i in $(seq 1 100)
 do
-    echo "GAME ${i}: $(python3 -m referee -s 250 -t 180 agent agentRandom -v 0)" | tee -a "$output_file"
+    echo "GAME ${i}: $(python3 -m referee -s 250 -t 180 -d agent agentGreedy -v 0)" | tee -a "$output_file"
 done
